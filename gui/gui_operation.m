84f66129e7a8b29ebfc8eec0d3f162a47ab85b74
zcc <listenzcc@mail.bnu.edu.cn>
2018-11-28 20:39:06 +0800

    v05, gui functional block design

--- gui/gui_operation.m
+++ gui/gui_operation.m
@@ -22,7 +22,7 @@ function varargout = gui_operation(varargin)
 
 % Edit the above text to modify the response to help gui_operation
 
-% Last Modified by GUIDE v2.5 28-Nov-2018 11:09:06
+% Last Modified by GUIDE v2.5 28-Nov-2018 18:38:35
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -198,3 +198,289 @@ function slider2_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+set(hObject, 'UserData', containers.Map)
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+num1 = str2double(get(handles.edit1, 'String'));
+if isnan(num1)
+    return
+end
+num2 = str2double(get(handles.edit2, 'String'));
+if isnan(num2)
+    return
+end
+
+str_nums = sprintf('%d, %d', num1, num2);
+set_map = get(handles.listbox2, 'UserData');
+set_map(str_nums) = 1;
+set(handles.listbox2, 'UserData', set_map)
+
+update_block_design(handles)
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+num = str2double(get(hObject, 'String'));
+if isnan(num)
+    num = 180;
+    set(hObject, 'String', num2str(num))
+end
+update_block_design(handles)
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function update_block_design(handles)
+set(handles.text11, 'String', '')
+
+% update hrf
+object = handles.axes2;
+contents = cellstr(get(handles.popupmenu4, 'String'));
+s = contents{get(handles.popupmenu4, 'Value')};
+hrf = eval(s);
+plot(object, hrf)
+set(object, 'Box', 'off')
+set(object, 'XTick', '')
+set(object, 'YTick', '')
+title(strrep(s, '_', '\_'))
+
+% update block_design
+object = handles.axes1;
+num = str2double(get(handles.edit3, 'String'));
+block_design = zeros(floor(num), 1);
+
+set_map = get(handles.listbox2, 'UserData');
+cell_map = keys(set_map);
+for j = 1 : length(cell_map)
+    nums = strsplit(cell_map{j}, ', ');
+    num1 = str2double(nums{1});
+    num2 = str2double(nums{2});
+    if num1 < 0
+        set(handles.text11, 'String', sprintf('（%s）非法，请检查！', cell_map{j}))
+    end
+    if num2 < 0
+        set(handles.text11, 'String', sprintf('（%s）非法，请检查！', cell_map{j}))
+    end
+    if num1 + num2 > length(block_design)
+        set(handles.text11, 'String', sprintf('（%s）非法，请检查！', cell_map{j}))
+    end
+    if sum(block_design(num1:(num1+num2))) > 0
+        set(handles.text11, 'String', sprintf('（%s）处检测到冲突，请检查！', cell_map{j}))
+    end
+    block_design(num1:(num1+num2)) = 1;
+end
+
+plot(object, conv(block_design, hrf, 'same'))
+set(object, 'Box', 'off')
+set(object, 'YTick', '')
+set(object, 'XLim', [1, num])
+xtick = linspace(0, num, 5);
+xtick(1) = 1;
+set(object, 'XTick', xtick)
+
+% update listbox2, block design listbox
+object = handles.listbox2;
+set_map = get(object, 'UserData');
+set(object, 'String', keys(set_map))
+contents = cellstr(get(object, 'String'));
+if ~isempty(contents)
+    try
+        str = contents{get(object, 'Value')};
+    catch
+        set(object, 'Value', set_map.Count)
+        str = contents{get(object, 'Value')};
+    end
+    nums = strsplit(str, ', ');
+    num1 = str2double(nums{1});
+    set(handles.edit1, 'String', num2str(num1))
+    num2 = str2double(nums{2});
+    set(handles.edit2, 'String', num2str(num2))
+    axe1 = handles.axes1;
+    yl = get(axe1, 'YLim');
+    hold on
+    line(axe1, [num1, num1], yl, 'color', 'black')
+    line(axe1, [num1+num2, num1+num2], yl, 'color', 'black')
+    rectangle(axe1, 'Position', [num1, yl(1), num2, yl(2)-yl(1)],...
+        'facecolor', [0.5, 0.5, 0.5, 0.5],...
+        'edgecolor', [0.5, 0.5, 0.5, 1])
+    hold off
+end
+
+% update popupmenu4, hrf popupmenu
+object = handles.popupmenu4;
+set_map = get(object, 'UserData');
+set(object, 'String', keys(set_map))
+
+
+% --- Executes on selection change in popupmenu4.
+function popupmenu4_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu4 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu4
+update_block_design(handles)
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+set_map = containers.Map;
+set_map('spm_hrf(2)') = 1;
+set(hObject, 'UserData', set_map)
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+set_map = get(handles.popupmenu4, 'UserData');
+str = get(hObject, 'String');
+try
+    hrf = eval(str);
+catch
+    set(handles.text11, 'String', sprintf('%s 非法，请检查！', str))
+    return
+end
+sum(hrf)
+if abs(sum(hrf)-1) > 0.1
+    warndlg(sprintf('%s 积分不为1，不符合HRF函数性质，请谨慎使用！', str))
+end
+set_map(str) = 1;
+set(handles.popupmenu4, 'UserData', set_map)
+update_block_design(handles)
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set_map = get(handles.listbox2, 'UserData');
+if set_map.Count == 0
+    return
+end
+contents = cellstr(get(handles.listbox2, 'String'));
+remove(set_map, contents{get(handles.listbox2, 'Value')})
+set(handles.listbox2, 'UserData', set_map)
+update_block_design(handles)
+
+% --- Executes on selection change in listbox2.
+function listbox2_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox2 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox2
+
+update_block_design(handles)
